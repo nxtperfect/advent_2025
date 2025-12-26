54c4f4a8850ba53fd6bc534a9b592b2e92fc40af
NxtPerfect <alakaxan@protonmail.com>
2025-12-26 17:35:38 +0100

    feat: part 2 by hyperneutrino

--- day05/solution.py
+++ day05/solution.py
@@ -2,10 +2,6 @@ import numpy as np
 
 
 def part1():
-    # ranges
-    # if empty line = no more ranges
-    # check if number in range
-    # count how many
     res = 0
     ranges, numbers = [], []
     with open("input.txt") as f:
@@ -27,11 +23,28 @@ def part1():
 
 def part2():
     res = 0
-    with open("test_input.txt") as f:
-        pass
-    assert res == 0
+    ranges, numbers = [], []
+    with open("input.txt") as f:
+        ranges, numbers = f.read().split("\n\n")
+        ranges = [list(map(int, r.split("-"))) for r in ranges.splitlines()]
+        numbers = list(map(int, numbers.splitlines()))
+        ranges.sort()
+
+        last = None
+
+        for low, high in ranges:
+            if last is None:
+                last = (low, high)
+                continue
+            if last[1] < low:
+                res += last[1] - last[0] + 1
+                last = (low, high)
+                continue
+            last = (last[0], max(last[1], high))
+        res += last[1] - last[0] + 1
     print(res)
+    assert res == 14
 
 
-part1()
-# part2()
+# part1()
+part2()
